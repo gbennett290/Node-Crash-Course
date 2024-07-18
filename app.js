cd9ea7a0b46e9cfc157dec34e4e928f8ccd22acd
Gabriel Bennett <gbennett29@ivytech.edu>
2024-07-18 11:17:29 -0400

    lesson 10 updates to allow deletion of blogs

--- app.js
+++ app.js
@@ -17,6 +17,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
+app.use(express.urlencoded({ extended: true}));
+
 app.use(morgan('dev'));
 
 app.get('/', (req, res) =>{
@@ -36,6 +38,41 @@ app.get('/blogs', (req, res) => {
         .catch((err) => {
             console.log(err);
         })
+})
+
+app.post('/blogs', (req, res) => {
+    const blog = new Blog(req.body)
+
+    blog.save()
+        .then((result) => {
+            res.redirect('/blogs');
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+})
+
+app.get('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+    Blog.findById(id)
+        .then(result => {
+           res.render('details', {blog: result, title: 'Blog Details' });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+})
+
+app.delete('/blogs/:id', (req, res) => {
+    const id = req.params.id;
+
+    Blog.findByIdAndDelete(id)
+    .then(result => {
+        res.json({ redirect: '/blogs '})
+    })
+    .catch(err => {
+        conbsole.log(err);
+    })
 })
 
 app.get('/blogs/create', (req, res) => {
